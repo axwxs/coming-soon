ccfaca6dc8a740ce614515914013e5b373dcaac7
axwxs <emailalirashid@gmail.com>
2023-04-05 00:53:56 -0400

    Added node + express parts

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+
+//app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    // res.send('Hello World!');
+    res.sendFile(path.join(__dirname, '/coming-soon.html'));
+
+});
+
+app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
