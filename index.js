a5ca5ec4e94e730966a1f7f0f7eb195a487a292e
axwxs <emailalirashid@gmail.com>
2023-05-14 03:41:54 +0000

    Test from server

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-//app.use(express.static('public'));
+// Updating file from server for testing purposes
 
 app.get('/', (req, res) => {
     // res.send('Hello World!');
